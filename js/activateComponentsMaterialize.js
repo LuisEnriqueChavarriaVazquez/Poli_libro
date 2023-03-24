299dd26d6b83b0c82ddaa844c5d511ef2ec0e82a
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-24 15:09:44 -0600

    Segunda prueba

---
+++ js/activateComponentsMaterialize.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+    $('.tabs').tabs();
+    $('.scrollspy').scrollSpy();
+    $('.materialboxed').materialbox();
+    $('.modal').modal();
+});
