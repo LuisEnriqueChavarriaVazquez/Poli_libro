9353749ae7f02604e2c2e602c6dadd0217c34338
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-25 13:14:31 -0600

    Primera prueba del modo oscuro

--- js/modoLuz.js
+++ js/modoLuz.js
@@ -3,21 +3,46 @@ let modOscuro = document.getElementById('modOscuro');
 let modLuminoso = document.getElementById('modLuminoso');
 let modSepia = document.getElementById('modSepia');
 
-//Identificador para acceder a la ruta del color
+//Identificador para acceder a la ruta del color (ruta actual y futura)
 let rutaColor = document.getElementById('colorAdaptable');
 
-modOscuro.addEventListener('click', () => {
-    M.toast({html: 'Modo oscuro activo'});
-    rutaColor.setAttribute('href', 'css/colors/standarDark.css');
-});
+//Verifcamos que la ruta no este en memoria
+let rutaSaved = localStorage.getItem('rutaSavedOn');
+if (rutaSaved === null){
+    localStorage.setItem('rutaSavedOn', 'css/colors/standarColors.css');
+}
 
-modLuminoso.addEventListener('click', () => {
-    M.toast({html: 'Modo luminoso activo'});
-    rutaColor.setAttribute('href', 'css/colors/standarColors.css');
-});
+//Validamos que los botones existan
+if(modOscuro !== null && modLuminoso !== null && modSepia !== null){
+    
+    //Definimos el color guardado en memoria
+    rutaColor.setAttribute('href', rutaSaved);
 
-modSepia.addEventListener('click', () => {
-    M.toast({html: 'Modo sepia activo'});
-    rutaColor.setAttribute('href', 'css/colors/standarSepia.css');
-});
+    //Boton para el modo oscuro
+    modOscuro.addEventListener('click', () => {
+        M.toast({html: 'Modo oscuro activo'});
+        localStorage.setItem('rutaSavedOn', 'css/colors/standarDark.css');
+        rutaColor.setAttribute('href', 'css/colors/standarDark.css');
+    });
+
+    //Boton para el modo con color normal
+    modLuminoso.addEventListener('click', () => {
+        M.toast({html: 'Modo luminoso activo'});
+        localStorage.setItem('rutaSavedOn', 'css/colors/standarColors.css');
+        rutaColor.setAttribute('href', 'css/colors/standarColors.css');
+    });
+
+    //Boton para los colores en sepia
+    modSepia.addEventListener('click', () => {
+        M.toast({html: 'Modo sepia activo'});
+        localStorage.setItem('rutaSavedOn', 'css/colors/standarSepia.css');
+        rutaColor.setAttribute('href', 'css/colors/standarSepia.css');
+    });
+    
+}else{
+
+    //En caso de que no haya botones solo agregamos el color guardado en memoria
+    rutaColor.setAttribute('href', rutaSaved);
+
+}
 
