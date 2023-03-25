91ac68e6b61830a80f2d98ba62b923fa6c21871b
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-24 20:33:00 -0600

    Actualización de paleta

--- js/browserStyle.js
+++ js/browserStyle.js
@@ -14,12 +14,9 @@ window.onscroll = function () {
         
 
         buttonBrowser.classList.add('buttonBrowserOwn-fixed');
-        buttonBrowser.classList.add('grey');
-        buttonBrowser.classList.add('lighten-2');
-        buttonBrowser.classList.remove('darken-4');
+        buttonBrowser.classList.add('cl5');
         buttonBrowser.classList.add('black-text');
-        buttonBrowser.classList.remove('light-blue');
-        buttonBrowser.classList.remove('white-text');
+        buttonBrowser.classList.remove('cl3');
 
         inputBrowser.classList.add('browserContainer-inputContainer-input-fixed');
         
@@ -28,12 +25,9 @@ window.onscroll = function () {
         
 
         buttonBrowser.classList.remove('buttonBrowserOwn-fixed');
-        buttonBrowser.classList.remove('grey');
-        buttonBrowser.classList.remove('lighten-2');
-        buttonBrowser.classList.add('darken-4');
+        buttonBrowser.classList.remove('cl5');
         buttonBrowser.classList.remove('black-text');
-        buttonBrowser.classList.add('light-blue');
-        buttonBrowser.classList.add('white-text');
+        buttonBrowser.classList.add('cl3');
 
         inputBrowser.classList.remove('browserContainer-inputContainer-input-fixed');
     }
