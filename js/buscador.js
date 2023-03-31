9d9f8025d7d2cefe9455162bbc6255a6a1ebba89
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-03-31 07:33:39 -0600

    Actualización del menu lateral, de los iconos y del glosario

--- js/buscador.js
+++ js/buscador.js
@@ -3,12 +3,23 @@ function buscar_concepto(){
     criterio=criterio.toLowerCase();
     let items=$('.conceptCard')
 
+    //Caja de conceptos
+    let parentConceptsGlosario = document.getElementById('parentConceptsGlosario');
+    
+    //Contenido cuando no se encuentra nada
+    let noResultContent = document.getElementById('noResultContent');
+
     for(item=0;item<items.length;item++){
         if (!items[item].innerHTML.toLowerCase().includes(criterio)) {
             items[item].style.display="none";
-        }
-        else {
-            items[item].style.display="grid";               
+
+            //Card de no hay resultados
+            noResultContent.style.display = "grid";  
+        }else {
+            items[item].style.display="grid";
+            
+            //Quitamos la card de buscador
+            noResultContent.style.display = "none";              
         }
     }
 }
