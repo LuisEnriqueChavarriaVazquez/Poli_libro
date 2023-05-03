dde9b3d1adf13c332fa2ca58bc92bfae1ec508b5
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-05-03 16:59:31 -0600

    Se agrego un boton flotante para descarga de resultados y validacion en caso de que el usario no haya acompletado su nombre y grupo en la evalaucion

--- js/evaluacion/validarCompletadoDeCuestionario.js
+++ js/evaluacion/validarCompletadoDeCuestionario.js
@@ -5,7 +5,12 @@ function validateExamenCompletado(){
     if(contadorPreguntasRespondidas.textContent < 19){
         $('#contenedorDescargaArchivos').hide();
     }else{
+        //Mostramo la casilla de descargas en la parte inferior
         $('#contenedorDescargaArchivos').show();
+
+        //Mostramos el boton en el costado derecho para bajar los resultados
+        let documentoPruebaFloat = document.getElementById('documentoPruebaFloat');
+        documentoPruebaFloat.classList.remove('scale-out');
         //Retroalimentamos al usuario
         M.toast({html: `En la parte inferior es posible descargar sus resultados.`});
     }
